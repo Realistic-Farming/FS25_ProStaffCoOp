e0a6d14760292a16795811787bb62b2a56840a51
tison <tison@example.com>
2026-09-08 11:18:34 +0200

    chore: remove unused icon source, fix build include xml dir

--- build.sh
+++ build.sh
@@ -32,7 +32,7 @@ py -c "
 import zipfile, os, sys
 
 zip_name = sys.argv[1]
-include = ['modDesc.xml', 'icon.dds', 'main.lua', 'src', 'translations']
+include = ['modDesc.xml', 'icon.dds', 'main.lua', 'src', 'translations', 'xml']
 
 with zipfile.ZipFile(zip_name, 'w', zipfile.ZIP_DEFLATED) as zf:
     for entry in include:
